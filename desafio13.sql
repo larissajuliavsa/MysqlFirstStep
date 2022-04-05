dc9925e81bdfc49f137fbb338d643285926eca9e
Larissa Araujo <larissa.jvsa@gmail.com>
2022-04-05 18:43:33 -0300

    feat: :sparkles: exibindo dados da coluna supplier_id que seja 1 ou 3

--- desafio13.sql
+++ desafio13.sql
@@ -2,4 +2,4 @@ SELECT supplier_id FROM northwind.purchase_orders
 WHERE supplier_id IN
 (1,3);
 
--- para que filtre a coluna submitted_date com valores 1 ou 3, utilizei WHERE IN
+-- para que filtre a coluna supplier_id com valores 1 ou 3, utilizei WHERE IN
