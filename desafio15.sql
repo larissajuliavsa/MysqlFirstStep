3a16c21085fbc562a9b2fc9f40177bc75400d3f4
Larissa Araujo <larissa.jvsa@gmail.com>
2022-04-05 19:05:01 -0300

    feat: :sparkles: exibindo horas da coluna submitted_hour

---
+++ desafio15.sql
@@ -0,0 +1,3 @@
+SELECT HOUR(submitted_date) AS submitted_hour FROM northwind.purchase_orders;
+
+-- para exibir somente as horas da coluna submitted_date, utilizei a função HOUR e renomei para submitted_hour
