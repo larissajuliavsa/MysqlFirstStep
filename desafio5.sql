d1be9c4e8edbfa91b26a5fac49cba867a8134e2b
Larissa Araujo <larissa.jvsa@gmail.com>
2022-04-05 17:03:57 -0300

    feat: :sparkles: exibindo dados da linha 4 a 13 da tabela products

---
+++ desafio5.sql
@@ -0,0 +1,4 @@
+SELECT * FROM northwind.products LIMIT 10 OFFSET 3;
+
+-- para exibir da linha 4 em diante, utilizei o OFFSET 3 que "pula" as 3 primeiras linhas
+-- para exibir até a linha 13, utilizei o LIMIT 10 pegando a quantidade de linhas necessárias
