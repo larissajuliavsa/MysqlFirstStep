9a8e301ab7166996471f1a2b24ea5a186db1d944
Larissa Araujo <larissa.jvsa@gmail.com>
2022-04-05 17:30:16 -0300

    feat: :sparkles: exibir os 5 últimos registros da coluna id

---
+++ desafio7.sql
@@ -0,0 +1,4 @@
+SELECT id FROM northwind.products ORDER BY id DESC LIMIT 5;
+
+-- para exibir apenas os 5 últimos dados da coluna id, filtrei por ordem decrescente em ORDER BY id DESC
+-- para exibir apenas os 5 dados, LIMIT 5
